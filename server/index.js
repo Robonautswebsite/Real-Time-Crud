4f565e5501d7cac706f7af72dc4b347ebcde44e0
Prateek Niket <prateekniket25@gmail.com>
2022-05-27 21:57:25 +0530

    init commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const server = http.createServer(app)
+
+const port = process.env.PORT
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    }
+})
+
+server.listen(5000, () => {
+    console.log("SERVER IS UP ON PORT", port)
+})
